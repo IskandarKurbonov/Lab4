97a3ba2279d52ddb478b2b5fbc9971b9375bd2b8
Iskandar <116521281+IskandarKurbonov@users.noreply.github.com>
2022-11-30 13:31:53 +0300

    commit v1.4

--- md5.py
+++ md5.py
@@ -103,7 +103,7 @@ class TestMD5(unittest.TestCase):
         f = open('md5.txt', 'r')
         text = f.read()
         self.md5.do_hash(text)
-        expected_md5 = 'a252fa342cb48974ecb13eb9c1136111'
+        expected_md5 = '905089ee33136caeeb6f9d107d3c6006'
         actual_md5 = self.md5.hex_digest()
         self.assertEqual(actual_md5, expected_md5)
         f.close()
